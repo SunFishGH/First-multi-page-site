08348ce31afc992f87f3bea2fccb1d8da22b1407
Denis <frontendsunfish@gmail.com>
2021-10-28 18:48:16 +0300

    Project page plus

---
+++ src/assets/js/lighting-mousemove.js
@@ -0,0 +1,7 @@
+/* const glow = document.querySelector('.glow');
+const honeycomb = document.querySelector('.honeycomb');
+
+honeycomb.addEventListener('mousemove', (e) =>{
+    glow.style.left = `${e.clientX - 400}px`;
+    glow.style.top = `${e.clientY - 175}px`;
+}); */
